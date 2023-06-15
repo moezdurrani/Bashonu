3b010d22c2016963d05e0fd69a0f86994602aaee
Moez <moezdurrani1@gmail.com>
2023-06-15 14:45:02 -0700

    files transferred

---
+++ music-list.js
@@ -0,0 +1,60 @@
+let allMusic = [
+
+    {
+        name: "Asheqi Angar ki Birai",
+        artist: "Irfan Ali Taj",
+        img: "music-1",
+        src: "music-1",
+        lyricsFile: "let_it_be.txt"
+    },
+    {
+        name: "Dunya Ju Baso",
+        artist: "Irfan Ali Taj",
+        img: "music-2",
+        src: "music-2",
+        lyricsFile: "see_you_again.txt"
+    },
+    {
+        name: "Dunya Nasha Hoi Tu Nasee Nisiko",
+        artist: "Muhsin Hayat Shadab",
+        img: "music-3",
+        src: "music-3",
+        lyricsFile: "bohemian_rhapsody.txt"
+    },
+    {
+        name: "Ka Dochaa Gti Ashru Daryah Birai",
+        artist: "Muhammad Faizan",
+        img: "music-4",
+        src: "music-4",
+        lyricsFile: "let_it_be.txt"
+    },
+    {
+        name: "Awa Pii Pii Nasha",
+        artist: "Mansoor Shabab",
+        img: "music-5",
+        src: "music-5",
+        lyricsFile: "see_you_again.txt"
+    },
+    {
+        name: "Asheq Majboor He Raheen Goi",
+        artist: "Ansar Elahi",
+        img: "music-6",
+        src: "music-6",
+        lyricsFile: "bohemian_rhapsody.txt"
+    },
+    {
+        name: "Last one",
+        artist: "Unknown",
+        img: "music-6",
+        src: "music-6",
+        lyricsFile: "let_it_be.txt"
+    },
+    {
+        name: "Last one",
+        artist: "Unknown",
+        img: "music-6",
+        src: "music-6",
+        lyricsFile: "see_you_again.txt"
+    },
+    
+    ];
