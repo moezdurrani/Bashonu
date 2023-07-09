bf7670254ca3968240eb0dcdbad8893475378ba8
Moez <moezdurrani1@gmail.com>
2023-07-09 13:57:58 -0700

    names

--- music-list.js
+++ music-list.js
@@ -2,8 +2,8 @@ let allMusic = [
 
     {
         name: "Asheqi Angar ki Birai",
-        writer: "Baba Siyar",
-        singer: "Don't know yet",
+        writer: "Mir Saleem / Irfan Ali Taj / Zoe Viccaji",
+        singer: "Irfan A. Taj / Zoe Viccaji",
         img: "music-1",
         src: "music-1",
         lyricsFile: "asheqi_angar.txt"
@@ -11,7 +11,7 @@ let allMusic = [
     {
         name: "Dunya Ju Baso",
         writer: "Irfan Ali Taj",
-        singer: "Don't know yet",
+        singer: "Irfan Ali Taj / Zoe Viccaji",
         img: "music-2",
         src: "music-2",
         lyricsFile: "dunya_ju_baso.txt"
@@ -19,15 +19,15 @@ let allMusic = [
     {
         name: "Awa Ta Dunyo Pechitam",
         writer: "Unknown",
-        singer: "Don't know yet",
+        singer: "Unknown",
         img: "music-3",
         src: "music-3",
         lyricsFile: "awa_ta_dunyo_pechitam.txt"
     },
     {
         name: "Ishq Daryah",
-        writer: "Unknown",
-        singer: "Don't know yet",
+        writer: "Irfan Ali Taj",
+        singer: "Irfan Ali Taj",
         img: "music-4",
         src: "music-4",
         lyricsFile: "ishq_daryah.txt"
@@ -35,15 +35,15 @@ let allMusic = [
     {
         name: "Khosh Bigim",
         writer: "Unknown",
-        singer: "Don't know yet",
+        singer: "Unknown",
         img: "music-5",
         src: "music-5",
         lyricsFile: "khosh_bigim.txt"
     },
     {
         name: "Scholote Biniyan Ma Xano Yaraan",
-        writer: "Unknown",
-        singer: "Don't know yet",
+        writer: "Abdul Hayat",
+        singer: "Unknown",
         img: "music-6",
         src: "music-6",
         lyricsFile: "scholote_biniyan.txt"
@@ -51,15 +51,15 @@ let allMusic = [
     {
         name: "Dashtu Sehra Asum Thrushni Armaan o sum",
         writer: "Unknown",
-        singer: "Don't know yet",
+        singer: "Unknown",
         img: "music-6",
         src: "music-6",
         lyricsFile: "dashtu_sehra_asum.txt"
-    },  
+    },   
     {
-        name: "Payan Alanio Lowah",
+        name: "Payah Alanio Lowah",
         writer: "Unknown",
-        singer: "Don't know yet",
+        singer: "Unknown",
         img: "music-6",
         src: "music-6",
         lyricsFile: "lowah.txt"
